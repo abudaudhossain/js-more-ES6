94c10c6a66ec032b42de99b4d20c6a5b2b2977d1
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-24 14:50:17 +0600

    add more es6 topics

---
+++ chaining.js
@@ -0,0 +1,4 @@
+const myObject = { x: 45, y: 12, z: 99, a: "ratul mullla", b: 'koribulla' }
+const { x, a } = myObject;
+
+console.log(x, a);
---
+++ destructuring.js
@@ -0,0 +1,19 @@
+const company = {
+    name :"Bosundhara",
+    productQunatity: 20,
+    productPrice:{
+        tishupaper: 17,
+        A4paper: 250,
+        oil: 120
+    },
+    address:{
+        roadName: "Jumanparak",
+        roadNo: '3/5'
+    },
+    ceo: "Paletham bucira",
+    phone:'017234556632',
+}
+
+const {phone,ceo,name,productQunatity}= company;
+
+console.log(ceo, phone, name, productQunatity);
