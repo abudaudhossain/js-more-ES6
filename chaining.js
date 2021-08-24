2f19fbdc147660f61bf04ab993a1d1355a696ba2
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-24 15:15:58 +0600

    add chainning distructuring

--- chaining.js
+++ chaining.js
@@ -1,4 +1,32 @@
 const myObject = { x: 45, y: 12, z: 99, a: "ratul mullla", b: 'koribulla' }
 const { x, a } = myObject;
 
-console.log(x, a);
+// console.log(x, a);
+
+// Array destructuring 
+const [bestu, faltu,age,midtu] = ['rifat', 'bibat',34, 'tini'];
+// console.log(bestu);
+// console.log(faltu);
+// console.log(midtu);
+// console.log(age + 5);
+
+// optional sine(?)
+
+const myCompany = {
+    name: "Gramminphone",
+    ceo: "nikolas pritho",
+    web:{
+       pragramming: "js",
+        fremworek: 'react',
+        product: "fortend Websit",
+        works:{
+            cuntry: "Banglades",
+            outdore: "India",
+            empley: 36
+        }
+    }
+}
+
+const {cuntry} = myCompany?.web?.works;
+console.log(myCompany.web?.backend?.program);
+console.log(cuntry);
